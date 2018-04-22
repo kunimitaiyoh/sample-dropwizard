220515931b2d4873e512513b77158548d065caa7
KUNMI Taiyoh <kunimi.taiyoh@gmail.com>
2018-04-22 21:55:16 +0900

    eliminate redundant properties.

--- src/main/scala/com/example/sample/SampleApplication.scala
+++ src/main/scala/com/example/sample/SampleApplication.scala
@@ -1,21 +1,15 @@
 package com.example.sample
 
-import com.datasift.dropwizard.scala.jdbi.tweak.{OptionContainerFactory, ProductResultSetMapperFactory}
-import com.example.sample.api.User
+import com.datasift.dropwizard.scala.jdbi.tweak.ProductResultSetMapperFactory
 import com.example.sample.dao.UserDao
 import com.example.sample.resources.UsersResource
 import io.dropwizard.Application
 import io.dropwizard.db.DataSourceFactory
-import io.dropwizard.hibernate.HibernateBundle
 import io.dropwizard.jdbi.DBIFactory
 import io.dropwizard.migrations.MigrationsBundle
 import io.dropwizard.setup.{Bootstrap, Environment}
 
 object SampleApplication extends Application[SampleConfig] {
-  val hibernate: HibernateBundle[SampleConfig] = new HibernateBundle[SampleConfig](classOf[User]) {
-    override def getDataSourceFactory(configuration: SampleConfig): DataSourceFactory = configuration.database
-  }
-
   def main(args: Array[String]): Unit = {
     run(args:_*)
   }
@@ -26,8 +20,6 @@ object SampleApplication extends Application[SampleConfig] {
 
       override def getMigrationsFileName: String = "migrations.sql"
     })
-
-    bootstrap.addBundle(this.hibernate)
   }
 
   override def run(config: SampleConfig, environment: Environment) : Unit = {
