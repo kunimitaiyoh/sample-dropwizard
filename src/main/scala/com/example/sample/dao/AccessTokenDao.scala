6bc1812d229a618c1424df01f5b84a6961f14212
KUNMI Taiyoh <kunimi.taiyoh@gmail.com>
2018-04-25 09:36:29 +0900

    AccessTokenDao now extends Dao.

--- src/main/scala/com/example/sample/dao/AccessTokenDao.scala
+++ src/main/scala/com/example/sample/dao/AccessTokenDao.scala
@@ -5,7 +5,7 @@ import java.util.UUID
 
 import com.example.sample.api.{AccessToken, User}
 
-trait AccessTokenDao {
+trait AccessTokenDao extends Dao[AccessToken] {
   def find(id: UUID): Option[AccessToken]
   def issue(user: User, created: Instant): AccessToken
   def update(token: AccessToken, lastAccess: Instant): AccessToken
