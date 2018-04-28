479ac16ea6ce28875f5aeb3fd6ed9571c2d86aaf
KUNMI Taiyoh <kunimi.taiyoh@gmail.com>
2018-04-28 14:30:35 +0900

    add some constraints to migrations. add table "avatars".

--- src/main/resources/migrations.sql
+++ src/main/resources/migrations.sql
@@ -41,10 +41,35 @@ ALTER TABLE users CHANGE mail mail VARCHAR(127) NOT NULL;
 ALTER TABLE users ADD UNIQUE (mail);
 
 --changeset sample:6
-
 CREATE TABLE access_tokens (
     id VARCHAR(127) PRIMARY KEY,
     user_id INT REFERENCES users(id) ON DELETE CASCADE,
-    created  DATETIME NOT NULL,
+    created DATETIME NOT NULL,
     last_access DATETIME NOT NULL
+);
+
+--changeset sample:7
+ALTER TABLE articles CHANGE user_id user_id INT NOT NULL;
+ALTER TABLE comments CHANGE user_id user_id INT NOT NULL;
+ALTER TABLE comments CHANGE article_id article_id INT NOT NULL;
+
+--changeset sample:8
+ALTER TABLE articles ADD FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE;
+ALTER TABLE comments ADD FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE;
+ALTER TABLE comments ADD FOREIGN KEY (article_id) REFERENCES articles(id) ON DELETE CASCADE;
+
+--changeset sample:9
+ALTER TABLE access_tokens CHANGE user_id user_id INT NOT NULL;
+ALTER TABLE access_tokens ADD FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE;
+
+--changeset sample:10
+CREATE TABLE avatars (
+    name VARCHAR(127) PRIMARY KEY,
+    user_id INT NOT NULL UNIQUE,
+    data MEDIUMBLOB NOT NULL,
+    width INT NOT NULL,
+    height INT NOT NULL,
+    created DATETIME NOT NULL,
+    
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
