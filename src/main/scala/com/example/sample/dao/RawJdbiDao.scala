6c1ec49204188a8f495d775dadc64e5f11d48b5e
KUNMI Taiyoh <kunimi.taiyoh@gmail.com>
2018-04-28 14:31:42 +0900

    in RawJdbiDao, enable transaction and add method insertWithPrimaryKey.

--- src/main/scala/com/example/sample/dao/RawJdbiDao.scala
+++ src/main/scala/com/example/sample/dao/RawJdbiDao.scala
@@ -5,11 +5,8 @@ import collection.JavaConverters._
 
 abstract class RawJdbiDao[T <: Product](val dbi: DBI) extends Dao[T] {
   def insert(values: Map[String, Any]): Int = {
-    val columns = values.keys.toList.sorted
-    val params = columns.map(":" + _).mkString(", ")
-    val statement = s"Insert into ${this.tableName} (${columns.mkString(", ")}) values ($params)"
-    this.dbi.withHandle(handle => {
-      handle.createStatement(statement)
+    this.dbi.inTransaction((handle, status) => {
+      handle.createStatement(insertionStatement(values))
         .bindFromMap(values.asJava)
         .executeAndReturnGeneratedKeys()
         .first()
@@ -20,5 +17,20 @@ abstract class RawJdbiDao[T <: Product](val dbi: DBI) extends Dao[T] {
         .asInstanceOf[java.lang.Long]
         .toInt
     })
+  }
+
+  def insertWithPrimaryKey(values: Map[String, Any]): Int = {
+    this.dbi.inTransaction((handle, status) => {
+      handle.createStatement(insertionStatement(values))
+        .bindFromMap(values.asJava)
+        .execute()
+    })
+  }
+
+  protected def insertionStatement(values: Map[String, Any]): String = {
+    val columns = values.keys.toList.sorted
+    val params = columns.map(":" + _).mkString(", ")
+    val statement = s"Insert into ${this.tableName} (${columns.mkString(", ")}) values ($params)"
+    statement
   }
 }
