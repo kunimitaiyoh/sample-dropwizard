a496d0728769f7374e6c6842ad6d7bed25699112
KUNMI Taiyoh <kunimi.taiyoh@gmail.com>
2018-04-28 22:20:30 +0900

    add methods "find", "delete" to RawJdbiDao.

--- src/main/scala/com/example/sample/dao/RawJdbiDao.scala
+++ src/main/scala/com/example/sample/dao/RawJdbiDao.scala
@@ -1,11 +1,15 @@
 package com.example.sample.dao
 
-import org.skife.jdbi.v2.DBI
+import java.util
+
+import com.example.sample.dao.RawJdbiDao.Queryable
+import org.skife.jdbi.v2.{DBI, Query}
+
 import collection.JavaConverters._
 
 abstract class RawJdbiDao[T <: Product](val dbi: DBI) extends Dao[T] {
   def insert(values: Map[String, Any]): Int = {
-    this.dbi.inTransaction((handle, status) => {
+    this.dbi.inTransaction((handle, _) => {
       handle.createStatement(insertionStatement(values))
         .bindFromMap(values.asJava)
         .executeAndReturnGeneratedKeys()
@@ -20,9 +24,25 @@ abstract class RawJdbiDao[T <: Product](val dbi: DBI) extends Dao[T] {
   }
 
   def insertWithPrimaryKey(values: Map[String, Any]): Int = {
-    this.dbi.inTransaction((handle, status) => {
+    this.dbi.inTransaction((handle, _) => {
       handle.createStatement(insertionStatement(values))
         .bindFromMap(values.asJava)
+        .execute()
+    })
+  }
+
+  def find[U](key: (String, Any), mapper: Queryable => U): U = {
+    this.dbi.inTransaction((handle, _) => {
+      val query = handle.createQuery(s"Select * from ${this.tableName} where ${key._1} = :key")
+        .bind("key", key._2)
+      mapper(query)
+    })
+  }
+
+  def delete(key: (String, Any)): Int = {
+    this.dbi.inTransaction((handle, _) => {
+      handle.createStatement(s"Delete from ${this.tableName} where ${key._1} = :key")
+        .bind("key", key._2)
         .execute()
     })
   }
@@ -33,4 +53,8 @@ abstract class RawJdbiDao[T <: Product](val dbi: DBI) extends Dao[T] {
     val statement = s"Insert into ${this.tableName} (${columns.mkString(", ")}) values ($params)"
     statement
   }
+}
+
+object RawJdbiDao {
+  type Queryable = Query[util.Map[String, AnyRef]]
 }
